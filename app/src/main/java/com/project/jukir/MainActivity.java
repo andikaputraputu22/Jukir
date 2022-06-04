653c5bcf5436bea0fd16db0f4741f088365d8b56
I Putu Andika Putra <andikaputra@detik.com>
2022-06-04 17:47:16 +0800

    Add side menu

--- app/src/main/java/com/project/jukir/MainActivity.java
+++ app/src/main/java/com/project/jukir/MainActivity.java
@@ -8,26 +8,42 @@ import android.os.Bundle;
 import android.view.View;
 
 import com.project.jukir.databinding.ActivityMainBinding;
+import com.project.jukir.databinding.LayoutMainBinding;
+import com.project.jukir.databinding.SideMenuBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     private Context context;
     private ActivityMainBinding binding;
+    private LayoutMainBinding layoutMainBinding;
+    private SideMenuBinding sideMenuBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
+        layoutMainBinding = binding.layoutMain;
+        sideMenuBinding = binding.layoutSideMenu;
         View view = binding.getRoot();
         setContentView(view);
         context = this;
 
-//        binding.helloWorld.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Intent intent = new Intent(context, RegisterActivity.class);
-//                startActivity(intent);
-//            }
-//        });
+        initAction();
+    }
+
+    private void initAction() {
+        layoutMainBinding.photo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                binding.drawerLayout.openDrawer(binding.navigationView, true);
+            }
+        });
+
+        sideMenuBinding.close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                binding.drawerLayout.closeDrawer(binding.navigationView, true);
+            }
+        });
     }
 }
